268207d1637ee4e1c59dc9bf62ebd38202dc1e3e
jbeaulie <beaulieu.jake@epa.gov>
2019-11-15 13:25:09 -0500

    Added population estimate figures and variograms to .Rmd

--- scripts/masterLibrary.R
+++ scripts/masterLibrary.R
@@ -3,3 +3,11 @@ library(sf) # spatial data
 library(gstat) # lagged scatterplot
 library(spdplyr) # apply dplyr verbs to sp objects
 library(rmarkdown)
+
+
+# ENFORCE EPA FORMAT ON NLA NAMES
+toEPA <- function(X1){
+  names(X1) = tolower(names(X1))
+  names(X1) = gsub(pattern = c("\\(| |#|)|/|-|\\+|:|_"), replacement = ".", x = names(X1))
+  X1
+}
