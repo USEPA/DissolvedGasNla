0e32b08d5d43dbd173b496b619c4d85e3188507d
Beaulieu <Beaulieu.Jake@epa.gov>
2023-03-22 09:14:18 -0400

    setting up manuscript infrastructure

--- manuscript/manuscript_support.R
+++ manuscript/manuscript_support.R
@@ -70,42 +70,105 @@ toc()
 
 
 # FIGURES-----------------
-# eliminate black points from with WSA9_NAME legend
-# rename WSA9_NAME legend to Ecoregions
 
 
-ggplot() +
-  geom_sf(data = ecoR, color = NA, aes(fill = WSA9_NAME)) +
-  geom_sf(data = dg.sf %>% filter(!is.na(n2o.src.snk)), 
-          aes(size = dissolved.n2o.nmol, color = n2o.src.snk),
-          show.legend = "point") +
-  geom_sf(data = states, fill = NA, color = "cornsilk3", size = 0.1) +
-  # guide argument below removes points from the boxes in the ecoregion legend
-  # https://aosmith.rbind.io/2020/07/09/ggplot2-override-aes/
-  scale_fill_manual("Ecoregion", values = cols, 
-                    guide = guide_legend(override.aes = list(shape = NA))) +
-  scale_color_manual(values = c("white", "black"), name = "source/sink") +
-  scale_size(name = expression(N[2]*O~(nM)),
-             range = c(0.1, 10), # custom size range
-             breaks = c(1, 10, 25, 50, 100)) + # custom breaks
-  theme(legend.key.size = unit(0.4, "cm"), # size of boxes in legend
-        legend.title = element_text(size = 8))
-ggsave("manuscript/manuscript_figures/figure1.png", width = 8, height = 4, units = "in")
+# # Figure 1
+# # Commented to expedite script
+# ggplot() +
+#   geom_sf(data = ecoR, color = NA, aes(fill = WSA9_NAME)) +
+#   geom_sf(data = dg.sf %>% filter(!is.na(n2o.src.snk)), 
+#           aes(size = dissolved.n2o.nmol, color = n2o.src.snk),
+#           show.legend = "point") +
+#   geom_sf(data = states, fill = NA, color = "cornsilk3", size = 0.1) +
+#   # guide argument below removes points from the boxes in the ecoregion legend
+#   # https://aosmith.rbind.io/2020/07/09/ggplot2-override-aes/
+#   scale_fill_manual("Ecoregion", values = cols, 
+#                     guide = guide_legend(override.aes = list(shape = NA))) +
+#   scale_color_manual(values = c("white", "black"), name = "source/sink") +
+#   scale_size(name = expression(N[2]*O~(nM)),
+#              range = c(0.1, 10), # custom size range
+#              breaks = c(1, 10, 25, 50, 100)) + # custom breaks
+#   theme(legend.key.size = unit(0.4, "cm"), # size of boxes in legend
+#         legend.title = element_text(size = 8))
+# ggsave("manuscript/manuscript_figures/figure1.png", width = 8, height = 4, units = "in")
 
 
 
 # MANUSCRIPT DATA-----
 
-all_predictions %>%
-  group_by(WSA9, .draw) %>% # group by iteration
-  summarise(n2o = mean(n2o), # 500 means for each WSA9
-            sat = mean(n2osat),
-            n2o_d = mean(n2o - n2oeq)) %>% # 500 means for each WSA9
-  # now summarize to 1 statistic per WSA9
-  summarise(across(contains("mean"), list(estimate = ~ round(median(.x), 3),
-                                          LCL = ~round(quantile(.x, probs = 0.025), 3),
-                                          UCL = ~ round(quantile(.x, probs = 0.975), 3))))
-                     
-                     estimate = round(median(mean_n2o), 3), 
-             LCL = round(quantile(mean_n2o, probs = 0.025), 3),
-             UCL = round(quantile(mean_n2o, probs = 0.975), 3))
+# paragraph 1
+undersatN2oPercent <- round(((dg %>% dplyr::filter(n2o.src.snk == "sink") %>% 
+                         {nrow(.)} / dg %>% distinct(site.id) %>% 
+                         {nrow(.)}) * 100), 1)
+
+propN2oSinkMean <- all_predictions %>% 
+  group_by(.draw) %>% 
+  summarise(prop_sat = sum(n2osat < 1) / length(.row)) %>% 
+  summarise(estimate = round(median(prop_sat), 3) * 100) %>% 
+  pull()
+
+propN2oSinkMean <- all_predictions %>% 
+  group_by(.draw) %>% 
+  summarise(prop_sat = sum(n2osat < 1) / length(.row)) %>% 
+  summarise(estimate = round(median(prop_sat), 3) * 100) %>% 
+  pull()
+
+propN2oSinkLower <- all_predictions %>% 
+  group_by(.draw) %>% 
+  summarise(prop_sat = sum(n2osat < 1) / length(.row)) %>% 
+  summarise(LCL = round(quantile(prop_sat, probs = 0.025), 3) * 100) %>% 
+  pull()
+
+propN2oSinkUpper <- all_predictions %>% 
+  group_by(.draw) %>% 
+  summarise(prop_sat = sum(n2osat < 1) / length(.row)) %>% 
+  summarise(UCL = round(quantile(prop_sat, probs = 0.975), 3) * 100) %>% 
+  pull()
+
+propN2oSinkMeanWM <- all_predictions %>% 
+  group_by(WSA9, .draw) %>% 
+  summarise(prop_sat = sum(n2osat < 1) / length(.row)) %>% 
+  summarise(estimate = round(median(prop_sat), 3) * 100) %>% 
+  filter(WSA9 == "WMT") %>% 
+  select(estimate) %>% 
+  pull()
+
+
+propN2oSinkLowerWM <- all_predictions %>% 
+  group_by(WSA9, .draw) %>% 
+  summarise(prop_sat = sum(n2osat < 1) / length(.row)) %>% 
+  summarise(LCL = round(quantile(prop_sat, probs = 0.025), 3) * 100) %>% 
+  filter(WSA9 == "WMT") %>% 
+  pull()
+
+
+propN2oSinkUpperWM <- all_predictions %>% 
+  group_by(WSA9, .draw) %>% 
+  summarise(prop_sat = sum(n2osat < 1) / length(.row)) %>% 
+  summarise(UCL = round(quantile(prop_sat, probs = 0.975), 3) * 100) %>% 
+  filter(WSA9 == "WMT") %>% 
+  pull()
+
+propN2oSinkMeanNPL <- all_predictions %>% 
+  group_by(WSA9, .draw) %>% 
+  summarise(prop_sat = sum(n2osat < 1) / length(.row)) %>% 
+  summarise(estimate = round(median(prop_sat), 3) * 100) %>% 
+  filter(WSA9 == "NPL") %>% 
+  select(estimate) %>% 
+  pull()
+
+
+propN2oSinkLowerNPL <- all_predictions %>% 
+  group_by(WSA9, .draw) %>% 
+  summarise(prop_sat = sum(n2osat < 1) / length(.row)) %>% 
+  summarise(LCL = round(quantile(prop_sat, probs = 0.025), 3) * 100) %>% 
+  filter(WSA9 == "NPL") %>% 
+  pull()
+
+
+propN2oSinkUpperNPL <- all_predictions %>% 
+  group_by(WSA9, .draw) %>% 
+  summarise(prop_sat = sum(n2osat < 1) / length(.row)) %>% 
+  summarise(UCL = round(quantile(prop_sat, probs = 0.975), 3) * 100) %>% 
+  filter(WSA9 == "NPL") %>% 
+  pull()
